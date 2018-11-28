5c6c4a50f85388e5554b6b1bcd76f5163fc1fc69
matyas.kloub <matyas.kloub@seznam.cz>
2018-11-28 12:33:15 +0100

    init

---
+++ App1/App1.Android/CompassTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Essentials;
+
+namespace App1.Droid
+{
+    public class CompassTest
+    {
+        // Set speed delay for monitoring changes.
+        SensorSpeed speed = SensorSpeed.UI;
+
+        public CompassTest()
+        {
+            // Register for reading changes, be sure to unsubscribe when finished
+            Compass.ReadingChanged += Compass_ReadingChanged;
+        }
+
+        void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+        {
+            var data = e.Reading;
+            Console.WriteLine($"Reading: {data.HeadingMagneticNorth} degrees");
+            // Process Heading Magnetic North
+        }
+
+        public void ToggleCompass()
+        {
+            try
+            {
+                if (Compass.IsMonitoring)
+                    Compass.Stop();
+                else
+                    Compass.Start(speed);
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Some other exception has occurred
+            }
+        }
+    }
+}
